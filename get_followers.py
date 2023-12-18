7abff9d87f47ce4b179caa6e9463d8545852f427
Ivan Tolkunov <tolkunov.ivan.s@gmail.com>
2023-12-17 18:35:53 -0800

    test

--- get_followers.py
+++ get_followers.py
@@ -1,36 +1,36 @@
 from flask import Flask, request
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+# from selenium import webdriver
+# from selenium.webdriver.chrome.service import Service
+# from webdriver_manager.chrome import ChromeDriverManager
 import re
 from urllib.parse import quote_plus
 import os
 
 app = Flask(__name__)
 
-def get_follower_count(linkedin):
-  # Setup webdriver
-  s=Service(ChromeDriverManager().install())
-  driver = webdriver.Chrome(service=s)
-  linkedin_encoded = quote_plus(linkedin)
-  driver.get(f"https://www.google.com/search?q={linkedin_encoded}")
+# def get_follower_count(linkedin):
+#   # Setup webdriver
+#   s=Service(ChromeDriverManager().install())
+#   driver = webdriver.Chrome(service=s)
+#   linkedin_encoded = quote_plus(linkedin)
+#   driver.get(f"https://www.google.com/search?q={linkedin_encoded}")
 
-  # Wait for the page to load
-  driver.implicitly_wait(5)
+#   # Wait for the page to load
+#   driver.implicitly_wait(5)
 
-  # Get follower count
-  # Get the page's HTML content
-  html_content = driver.page_source
+#   # Get follower count
+#   # Get the page's HTML content
+#   html_content = driver.page_source
 
-  # Search for the follower count in the HTML content
-  match = re.search(r'\s*\d+(.\d+)?(K|M)?\+\s*followers', html_content)
+#   # Search for the follower count in the HTML content
+#   match = re.search(r'\s*\d+(.\d+)?(K|M)?\+\s*followers', html_content)
 
-  if match:
-    driver.quit()
-    return match.group()
-  else:
-    driver.quit()
-    return 0
+#   if match:
+#     driver.quit()
+#     return match.group()
+#   else:
+#     driver.quit()
+#     return 0
 
 @app.route('/get_follower_count', methods=['GET'])
 def get_follower_count_route():

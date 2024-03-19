f764e842eabcb6c97e5d95c2c43e1c940cb88b42
Ivan Tolkunov <tolkunov.ivan.s@gmail.com>
2024-03-19 15:25:05 -0700

    refactor code, added new lib

--- app.py
+++ app.py
@@ -1,57 +1,93 @@
-from urllib.request import urlretrieve
 from flask import Flask, request
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+from pydantic import BaseModel
 from urllib.parse import quote_plus
-from bs4 import BeautifulSoup
-from PIL import Image
-from pathlib import Path
-import imagehash
-import hashlib
-import re
 import os
 from functools import cache
+from pathlib import Path
+from PIL import Image
+import imagehash
+from urllib.request import urlretrieve
+from linkedin_scraper import actions
+from selenium.webdriver.common.by import By
+import pickle
+import hashlib
+
+
+email = "email"
+password = "password"
+cookies_file_path = Path("cookies") / "cookies.pkl"
 
 app = Flask(__name__)
 
-def get_follower_count(driver, linkedin):
-    linkedin_encoded = quote_plus(linkedin)
-    driver.get(f"https://www.google.com/search?q={linkedin_encoded}")
+class Request(BaseModel):
+    linkedin_url: str
 
-    driver.implicitly_wait(5)
 
-    html_content = driver.page_source
+class Response(BaseModel):
+    followers: int
+    profile_photo_url: str
+    profile_photo_hash: str
 
-    match = re.search(r'\s*\d+(.\d+)?(K|M)?\+\s*followers', html_content)
-
-    if match:
-        s = match.group().replace(' followers', '').replace('+', '')
-        return parse_number(s)
-    else:
-        return 0
-
-@cache
 def create_driver():
-    try:
-        s=Service(ChromeDriverManager().install())
-        return webdriver.Chrome(service=s)
-    except:
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--disable-dev-shm-usage")
-        chrome_options.add_argument("--no-sandbox")
-        return webdriver.Chrome(service=Service(os.environ.get("CHROMEDriver_PATH")), options=chrome_options)
+    from selenium import webdriver
 
-def parse_number(num):
-    if num[-1] == 'K':
-        return int(float(num[:-1]) * 1000)
-    elif num[-1] == 'M':
-        return int(float(num[:-1]) * 1000000)
-    else:
-        return int(num)
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--window-size=1920,1080")
+    return webdriver.Chrome(options=chrome_options)
+
+def get_info(driver, linkedin):
+    try:
+        login(driver)
+
+        driver.get(linkedin)
+
+        driver.implicitly_wait(5)
+
+        followers_el = driver.find_element(By.CSS_SELECTOR, '.text-body-small.t-black--light.inline-block>.t-bold')
+        img_el = driver.find_element(By.CSS_SELECTOR, '.pv-top-card-profile-picture__image--show')
+
+        followers = followers_el.text
+        img_url = img_el.get_attribute('src')
+
+        return followers, img_url
     
+    except Exception as e:
+        return str(e), None
+
+def save_cookie(driver):
+    with open(cookies_file_path, 'wb') as filehandler:
+        pickle.dump(driver.get_cookies(), filehandler)
+
+def login(driver):
+    cookies_file_path.parent.mkdir(parents=True, exist_ok=True)
+    if not os.path.isfile(cookies_file_path):
+       actions.login(driver, email, password)
+       save_cookie(driver)
+       return
+    
+    with open(cookies_file_path, 'rb') as cookiesfile:
+        cookies = pickle.load(cookiesfile)
+        for cookie in cookies:
+            if cookie['name'] == 'li_at':
+                actions.login(driver, email, password, cookie['value'])
+                return
+    
+    actions.login(driver, email, password)
+    save_cookie(driver)
+            
+def download_image(url):
+    file_name = Path("images") / (hashlib.sha256(url.encode()).hexdigest() + ".jpg")
+    file_name.parent.mkdir(parents=True, exist_ok=True)
+    urlretrieve(url, file_name)
+    return file_name
+
+def get_image_hash(img_url):
+    img_path = download_image(img_url)
+    return imagehash.phash(Image.open(img_path))
 
 @app.route('/get_follower_count', methods=['GET'])
 def get_follower_count_route():
@@ -59,19 +95,12 @@ def get_follower_count_route():
   if linkedin_url:
     try:
         driver = create_driver()
-        follower_count = get_follower_count(driver, linkedin_url)
-        img_link = get_img(driver, linkedin_url)
+        followers, img_url = get_info(driver, linkedin_url)
+        img_hash = get_image_hash(img_url)
+        print(followers, img_url, str(img_hash))
+        return {"followers": followers, "img_url": img_url, "img_hash": str(img_hash)}
     finally:
         driver.quit() 
-        
-    img_path = download_image(img_link)
-    hash = imagehash.phash(Image.open(img_path))
-    print("hash", hash)
-    return {"follower_count": follower_count,
-            "profile_img_link": img_link,    
-            "profile_img_hash_p": str(hash)}
-  else:
-    return "No LinkedIn URL provided", 400
 
 def download_image(url):
     file_name = Path("images") / (hashlib.sha256(url.encode()).hexdigest() + ".jpg")
@@ -79,35 +108,6 @@ def download_image(url):
     file_name.parent.mkdir(parents=True, exist_ok=True)
     urlretrieve(url, file_name)
     return file_name
-
- 
-def get_img(driver, linkedin):
-    driver.get(linkedin)
-
-    driver.implicitly_wait(5)
-
-    html_content = driver.page_source
-
-    if html_content:
-        soup = BeautifulSoup(html_content, 'html.parser')
-        divs = soup.find_all("div", {"class": "contextual-sign-in-modal__screen contextual-sign-in-modal__context-screen flex flex-col my-4 mx-3"})
-        img_link = img_from_divs(divs)
-        return img_link[0]
-    
-    return "No HTML content found"
-
-def img_from_divs(divs):
-    images = []
-    for div in divs:
-        img_tags = div.find_all('img')
-        for img in img_tags:
-            if img.has_attr('src'):
-                images.append(img['src'])
-    if len(images) == 0: 
-       return "No images found"
-   
-    return images
-   
 
 if __name__ == '__main__': 
    app.run(debug=True)
